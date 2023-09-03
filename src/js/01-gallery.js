aa4d8d1dfcb5c63d395710999cc197de6bbb66f1
Ewa Zawilińska <zawilecewa@gmail.com>
2023-09-03 14:32:23 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,23 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-import simpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
-const gallery = document.querySelector('.gallery');
 
-function createGallery(listImage) {
-  const gallery = listImage
-    .map(
-      image =>
-        `<li><div><a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a></div></li>`
-    )
-    .join('');
-
-  return gallery;
-}
-
-function addElement(callback, listElement, querySelector) {
-  querySelector.insertAdjacentHTML('beforeend', callback(listElement));
-}
-addElement(createGallery, galleryItems, gallery);
+const imagesGallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    galleryItem => `<div class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join('');
+imagesGallery.insertAdjacentHTML('beforeend', markup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,79 +1,21 @@
-//----------------------------- Imports -----------------------------------------------------
-import _ from 'lodash';
-//----------------------------- Const setting ------------------------------------------------------
-const keyFormLocalData = 'feedback-form-state';
-//----------------------------- Object -------------------------------------------
-const formData = {
-  eMail: '',
-  message: '',
-
-  clear() {
-    this.eMail = '';
-    this.message = '';
-  },
-};
-//------------------------------ Functions -----------------------------------------------------
-function setLocalData(data) {
-  try {
-    localStorage.setItem(keyFormLocalData, JSON.stringify(data));
-  } catch (error) {
-    console.log('Błąd podczas zapisu');
-    console.log(error);
-  }
-}
-function getLocalData(key, data) {
-  try {
-    const serializedState = localStorage.getItem(key);
-    const getData =
-      serializedState === null ? undefined : JSON.parse(serializedState);
-    data.eMail = getData.eMail;
-    data.message = getData.message;
-    return data;
-  } catch (error) {
-    if (length <= 0) {
-      console.log('Brak danych w lokalnym magazynie');
-    } else {
-      console.error('Get state error: ', error.message);
-    }
-  }
-
-  //data.eMail = 'dsdsds';
-  //data.message = 'ok';
-  //return data;
-}
-function removeLocalData(key) {
-  localStorage.removeItem(key);
-}
-function completeForm(eMail, message) {
-  form.email.value = eMail;
-  form.message.value = message;
-}
-function getFormLocalData() {
-  getFormLocalData(keyFormLocalData, formData);
-  completeForm;
-  completeForm(formData.email, formData.message);
-}
-function saveForm(eve) {
-  formData.eMail = form.email.value;
-  formData.message = form.message.value;
-  setLocalData(formData);
-}
-function resetAllData() {
-  removeLocalData(keyFormLocalData); // usuwanie danych formularza z localData
-  formData.clear(); // czyszczenie danych
-  completeForm(' ', ' '); // czyszczenie formularza
-}
-function sendForm(eve) {
-  eve.preventDefault();
-  console.log(formData); // wypisanie na konsoli email i message
-  resetAllData();
-}
-//-----------------------------------------------------------------------------------
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const { email, message } = form.elements;
 
-getLocalData(keyFormLocalData, formData);
-completeForm(formData.eMail, formData.message);
+form.addEventListener('input', throttle(getInputData, 500));
+form.addEventListener('submit', getEmailData);
 
-form.addEventListener('input', _.throttle(saveForm, 1000));
-form.addEventListener('submit', sendForm);
+function getInputData(event) {
+  const inputData = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
+}
+
+function getEmailData(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(email);
+  localStorage.removeItem(message);
+}
