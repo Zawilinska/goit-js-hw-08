50d161b13571578b7a767de803938782c190ccd4
Ewa Zawilińska <zawilecewa@gmail.com>
2023-09-03 15:02:18 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,30 @@
-var throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
 
-function saveTime(eve) {
-  localStorage.setItem('videoplayer-current-time', eve.seconds);
-}
-function loadingTime() {
-  const getTime = localStorage.getItem(`videoplayer-current-time`);
-  let time = parseInt(getTime);
-  return time;
-}
-function setPlayerTime(time) {
-  player
-    .setCurrentTime(time)
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-        default:
-          // some other error occurred
-          break;
-      }
-    });
-}
+player.on(
+  'timeupdate',
+  throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
 
-player.on('timeupdate', _.throttle(saveTime, 1000));
-setPlayerTime(loadingTime());
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
