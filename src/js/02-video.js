0fa3e6ff34fa45f79c96a43e2a5707b550939adf
Ewa Zawilińska <zawilecewa@gmail.com>
2023-09-06 21:27:29 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,19 +12,4 @@ player.on(
   }, 1000)
 );
 
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,10 +4,19 @@ const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const { email, message } = form.elements;
 
+window.addEventListener('load', () => {
+  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedData) {
+    const inputData = JSON.parse(savedData);
+    email.value = inputData.email;
+    message.value = inputData.message;
+  }
+});
+
 form.addEventListener('input', throttle(getInputData, 500));
 form.addEventListener('submit', getEmailData);
 
-function getInputData(event) {
+function getInputData() {
   const inputData = { email: email.value, message: message.value };
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
 }
@@ -16,6 +25,5 @@ function getEmailData(event) {
   event.preventDefault();
   console.log({ email: email.value, message: message.value });
   form.reset();
-  localStorage.removeItem(email);
-  localStorage.removeItem(message);
+  localStorage.removeItem(LOCALSTORAGE_KEY);
 }
